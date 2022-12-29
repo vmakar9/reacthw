205dbce0ec40d462f18439307251bfb4c9edd5c4
vmakar9 <zmerin212@gmail.com>
2022-12-29 17:02:38 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {w3cwebsocket as W3CWebSocket } from "websocket";
+import {useEffect, useState} from "react";
 
+const url = 'ws://localhost:8080/chat'
+const client = new W3CWebSocket(url)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let[msgs,setMessages]= useState([])
+
+    useEffect(()=>  {
+        client.onopen = (e)=> {
+            console.log('connected to chat',e)
+        };
+        client.onmessage = (e)=> {
+            console.log(e.data)
+            msgs.push(e.data)
+            setMessages([...msgs])
+        }
+    },[])
+
+
+return <div>
+
+    <form onSubmit={(e)=>  {
+        e.preventDefault()
+        let messagefound = e.target.message.value;
+        console.log(messagefound)
+        client.send(messagefound)
+
+    }
+    }>
+    <input type={"text"} name={'message'}/>
+    <button>send msg</button>
+</form>
+
+    {
+        msgs.map(value =>  <div>{value}</div>)
+    }
+</div>
+
 }
 
 export default App;
